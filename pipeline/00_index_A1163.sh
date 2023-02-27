11a5c904c2533a398855c9269ec022f7e8680162
Jason Stajich <jasonstajich.phd@gmail.com>
2023-02-27 15:06:00 -0800

    support for both genome strains

---
+++ pipeline/00_index_A1163.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/bash -l
+#SBATCH -p short -c 2 --mem 8gb
+module load samtools
+module load bwa
+if [ -f config.txt ]; then
+	source config.txt
+fi
+mkdir -p $GENOMEFOLDER
+pushd $GENOMEFOLDER
+# THIS IS EXAMPLE CODE FOR HOW TO DOWNLOAD DIRECT FROM FUNGIDB
+RELEASE=61
+SPECIES=AfumigatusA1163
+URL=https://fungidb.org/common/downloads/release-${RELEASE}/$SPECIES
+PREF=FungiDB-${RELEASE}_${SPECIES}
+FASTAFILE=${PREF}_Genome.fasta
+DOMAINFILE=${PREF}_InterproDomains.txt
+GFF=${PREF}.gff
+## THIS IS FUNGIDB DOWNLOAD PART
+echo "working off $FASTAFILE - check if these don't match may need to update config/init script"
+
+if [ ! -f $DOMAINFILE ]; then
+	curl -O $URL/txt/$DOMAINFILE
+fi
+if [ ! -f $FASTAFILE ] ; then
+	curl -O $URL/fasta/data/$FASTAFILE
+fi
+if [ ! -f $GFF ]; then
+	curl -O $URL/gff/data/$GFF
+fi
+
+if [[ ! -f $FASTAFILE.fai || $FASTAFILE -nt $FASTAFILE.fai ]]; then
+	samtools faidx $FASTAFILE
+fi
+if [[ ! -f $FASTAFILE.bwt || $FASTAFILE -nt $FASTAFILE.bwt ]]; then
+	bwa index $FASTAFILE
+fi
+
+DICT=$(basename $FASTAFILE .fasta)".dict"
+
+if [[ ! -f $DICT || $FASTAFILE -nt $DICT ]]; then
+	rm -f $DICT
+	samtools dict $FASTAFILE > $DICT
+	ln -s $DICT $FASTAFILE.dict 
+fi
+grep ">" $FASTAFILE | perl -p -e 's/>((Chr)?(\d+|mito)_\S+)\s+.+/$1,$3/' > A1163_chrom_nums.csv
+popd
